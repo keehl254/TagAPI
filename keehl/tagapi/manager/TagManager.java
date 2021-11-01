dffc759b2a2a01cffced6fc33ffd1a601a1370d9
Lane <lanekeehl@gmail.com>
2021-11-01 02:17:08 -0500

    Remove debug

--- keehl/tagapi/manager/TagManager.java
+++ keehl/tagapi/manager/TagManager.java
@@ -1,26 +1,13 @@
 package keehl.tagapi.manager;
 
 import keehl.tagapi.TagAPI;
-import keehl.tagapi.TagBuilder;
-import keehl.tagapi.api.Tag;
-import org.bukkit.ChatColor;
-import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TagManager extends JavaPlugin {
 
     @Override()
     public void onEnable() {
         TagAPI.onEnable(this);
-
-        TagAPI.setDefaultTag(EntityType.PIG, target ->
-                TagBuilder.create(target).withLine(pl -> target.getName()).withLine(pl -> ChatColor.YELLOW + "Hello " + pl.getName() + "!").build()
-        );
     }
 
     @Override()
