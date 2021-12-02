8cddcaa2f4274749466cb3764cdae411f564d92a
Keehl <roblox.keehl254@gmail.com>
2021-12-01 18:12:11 -0600

    Added 1.18 support + moved to built-in NBT system.

--- com/lkeehl/tagapi/manager/TagManager.java
+++ com/lkeehl/tagapi/manager/TagManager.java
@@ -11,13 +11,6 @@ public class TagManager extends JavaPlugin {
     @Override()
     public void onEnable() {
         TagAPI.onEnable(this);
-
-        TagAPI.setDefaultTag(EntityType.PIG,entity -> {
-            TagBuilder builder = TagBuilder.create(entity); // Create a new TagBuilder
-            builder.withLine(pl->"First Line").withLine(pl->"Second Line"); // Add a first and second line
-            builder.withLine(pl->"Third Line", pl->false); // Add another line, because why not?
-            return builder.build();
-        });
     }
 
     @Override()
